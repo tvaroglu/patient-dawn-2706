2e10a5adbd6a07e1ce3abff56da96f5b3ad1b05c
Taylor Varoglu <tava8398@colorado.edu>
2021-08-04 11:23:16 -0600

    Add completed User Story 2

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -2,5 +2,15 @@ class Competition < ApplicationRecord
 
   has_many :competition_teams
   has_many :teams, through: :competition_teams
-  
+
+  def player_statistics
+    teams.joins(:players)
+    .select("COUNT(players.*) AS total_player_count, SUM(players.age) AS total_player_age")
+    .group(:id)
+  end
+
+  def average_player_age
+    player_statistics.sum {|team| team.total_player_age}.to_f / player_statistics.sum {|team| team.total_player_count}
+  end
+
 end
--- spec/features/competitions/index_spec.rb
+++ spec/features/competitions/index_spec.rb
@@ -1,16 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe 'competitions index' do
-  it 'displays all competitions and attributes' do
-    competition = Competition.create!(name: 'PAC12 Championship', location: 'Denver', sport: 'football')
+  # User Story 1 - Competition Index
+    # As a user
+      # When I visit the competion index
+      # Then I see the names of all competitions
+      # Each competition name links to its show page
+  it 'displays all competitions and links to show pages' do
+    competition_1 = Competition.create!(name: 'PAC12 Championship', location: 'Denver', sport: 'football')
+    competition_2 = Competition.create!(name: 'March Madness', location: 'Houston', sport: 'basketball')
 
     visit '/competitions'
     expect(page).to have_content('All Competitions')
 
-    within "#competition-#{competition.id}" do
-      expect(page).to have_link(competition.name)
-      click_on(competition.name)
-      expect(current_path).to eq("/competitions/#{competition.id}")
+    within "#competition-#{competition_1.id}" do
+      expect(page).to have_link(competition_1.name)
+      click_on(competition_1.name)
+      expect(current_path).to eq("/competitions/#{competition_1.id}")
+    end
+
+    visit '/competitions'
+    within "#competition-#{competition_2.id}" do
+      expect(page).to have_link(competition_2.name)
+      click_on(competition_2.name)
+      expect(current_path).to eq("/competitions/#{competition_2.id}")
     end
   end
 
